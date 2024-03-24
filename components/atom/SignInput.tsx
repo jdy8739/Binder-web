9636d875206c18017efe50a25eef76659f448293
jdy8739 <jdy8739@naver.com>
2024-03-24 09:02:33 +0900

    Feat: dev atom SignInput

---
+++ components/atom/SignInput.tsx
@@ -0,0 +1,20 @@
+import { useId } from 'react';
+
+const SignInput = ({
+  label,
+  required = false,
+}: {
+  label: string;
+  required?: boolean;
+}) => {
+  const inputId = useId();
+
+  return (
+    <>
+      <label htmlFor={inputId}>{label}</label>
+      <input id={inputId} required={required} />
+    </>
+  );
+};
+
+export default SignInput;
