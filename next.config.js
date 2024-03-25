f67cb0d5f32d8008506d04945e660c795aa1059c
jdy8739 <jdy8739@naver.com>
2024-03-25 22:40:22 +0900

    Set: set next.config.js with sassOptions

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`,
+  },
+};
+
+module.exports = nextConfig;
