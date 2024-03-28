cb38273e1d1a94bcdbeb1f104fa63bdd2958bfb7
jdy8739 <jdy8739@naver.com>
2024-03-29 00:42:54 +0900

    Feat: make the alpha version of SignInput component

--- components/atoms/SignInput.tsx
+++ components/atoms/SignInput.tsx
@@ -1,19 +1,61 @@
-import { useId } from 'react';
+'use client';
+
+import { useId, useState } from 'react';
+
+import classNames from 'classnames/bind';
+
+import styles from './SignInput.module.scss';
+
+const cx = classNames.bind(styles);
 
 const SignInput = ({
-  label,
+  classname = '',
+  label = '',
+  placeholder = '',
+  helperText = '',
+  isError = false,
   required = false,
+  isPassword = false,
 }: {
+  classname?: string;
   label?: string;
+  placeholder?: string;
+  helperText?: string;
+  isError?: boolean;
   required?: boolean;
+  isPassword?: boolean;
 }) => {
   const inputId = useId();
 
+  const [isFocused, setIsFocused] = useState(false);
+
+  const [isPasswordType, setIsPasswordType] = useState(isPassword);
+
   return (
-    <>
+    <div className={cx('wrapper')}>
       <label htmlFor={inputId}>{label}</label>
-      <input id={inputId} required={required} />
-    </>
+      <input
+        id={inputId}
+        className={cx(classname, { error: isError })}
+        placeholder={placeholder}
+        required={required}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        type={isPasswordType ? 'password' : 'input'}
+        spellCheck={false}
+      />
+      <small className={cx({ error: isError && !isFocused })}>
+        {helperText}
+      </small>
+      {isPassword && (
+        <button
+          type="button"
+          onClick={() => setIsPasswordType(!isPasswordType)}
+        >
+          {isPasswordType ? 'eye' : 'slash'}
+        </button>
+      )}
+    </div>
   );
 };
 
