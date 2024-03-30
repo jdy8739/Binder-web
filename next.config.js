5243d7cfa9770406b2ae11c42c3ff676d6d3b24b
jdy8739 <jdy8739@naver.com>
2024-03-30 21:39:24 +0900

    Set: install svgr/webpack lib

--- next.config.js
+++ next.config.js
@@ -6,6 +6,21 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss"; @import "styles/_extentions.scss";`,
   },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    )
+    config.module.rules.push(
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack'],
+      },
+    )
+    fileLoaderRule.exclude = /\.svg$/i
+    return config
+  }
 };
 
 module.exports = nextConfig;
