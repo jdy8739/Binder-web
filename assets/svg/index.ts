97bbdcc5a052c8bfb2bca28e87bb6230a8faba3d
jdy8739 <jdy8739@naver.com>
2024-03-31 23:06:31 +0900

    Set: add svg files in asset / svg dir

--- assets/svg/index.ts
+++ assets/svg/index.ts
@@ -1,4 +1,7 @@
 import PwHidden from './pw_hidden.svg';
 import PwVisible from './pw_visible.svg';
+import Instagram from './instagram.svg';
+import Facebook from './facebook.svg';
+import Blof from './blof.svg';
 
-export { PwHidden, PwVisible };
+export { PwHidden, PwVisible, Instagram, Facebook, Blof };
