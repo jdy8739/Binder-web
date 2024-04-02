49b3cc6a8710fefeec711cc36a82f1706c3d39e4
jdy8739 <jdy8739@naver.com>
2024-04-03 01:41:32 +0900

    Style: create Radio comp and apply styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import SignInput from '../components/atoms/SignInput';
 import Button from '/components/atoms/Button';
+import CheckBox from '/components/atoms/CheckBox';
+import Radio from '/components/atoms/Radio';
 
 const Page = () => {
   return (
@@ -8,6 +10,11 @@ const Page = () => {
       <SignInput label="가가가" isPassword helperText="sds" required />
       <Button content="나나나" />
       <Button content="나나나" disabled />
+      <CheckBox />
+      <Radio name="hi" />
+      <Radio name="hi" />
+      <Radio name="hi" />
+      <Radio name="hi" />
     </>
   );
 };
