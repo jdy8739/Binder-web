39d616edc5aecdbd080c7a2a0d1af1b30af6191c
jdy8739 <jdy8739@naver.com>
2024-04-07 00:30:02 +0900

    Set: add Search svg

--- assets/svg/index.ts
+++ assets/svg/index.ts
@@ -4,5 +4,6 @@ import Instagram from './instagram.svg';
 import Facebook from './facebook.svg';
 import Blof from './blof.svg';
 import Check from './Check.svg';
+import Search from './Search.svg';
 
-export { PwHidden, PwVisible, Instagram, Facebook, Blof, Check };
+export { PwHidden, PwVisible, Instagram, Facebook, Blof, Check, Search };
