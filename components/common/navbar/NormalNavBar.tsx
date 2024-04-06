540c0f61c2c31acdeec9aa832bf582e9208cd099
jdy8739 <jdy8739@naver.com>
2024-04-07 00:29:29 +0900

    Feat: create Ui NormalNavBar comp

--- components/common/navbar/NormalNavBar.tsx
+++ components/common/navbar/NormalNavBar.tsx
@@ -1,5 +1,73 @@
+import Link from 'next/link';
+import classNames from 'classnames/bind';
+
+import SearchInput from '/components/atoms/SearchInput';
+
+import style from './NormalNavBar.module.scss';
+
+const cx = classNames.bind(style);
+
 const NormalNavBar = () => {
-  return <div />;
+  return (
+    <header className={cx('wrapper')}>
+      <div>
+        <div className={cx('left')}>
+          <span className={cx('logo')}>
+            <div>바인더</div>
+            <div className={cx('logo-border')}>
+              <div />
+              <div />
+            </div>
+            <small>
+              <div>
+                <span>Bio</span>
+                <span>-Industrial community</span>
+              </div>
+            </small>
+          </span>
+          <SearchInput className={cx('search-input')} />
+        </div>
+        <span className={cx('right')}>
+          <ul>
+            <li>
+              <Link href="/#" className={cx('sign-link')}>
+                로그인
+              </Link>
+            </li>
+            <li>
+              <Link href="/#" className={cx('sign-link')}>
+                회원가입
+              </Link>
+            </li>
+          </ul>
+        </span>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/#" className={cx('nav-link')}>
+              필드별
+            </Link>
+          </li>
+          <li>
+            <Link href="/#" className={cx('nav-link')}>
+              직무 게시판
+            </Link>
+          </li>
+          <li>
+            <Link href="/#" className={cx('nav-link')}>
+              학술 게시판
+            </Link>
+          </li>
+          <li>
+            <Link href="/#" className={cx('nav-link')}>
+              취업이직 게시판
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default NormalNavBar;
