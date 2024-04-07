7929922f9946b63db95719369ef1781272b96c17
jdy8739 <jdy8739@naver.com>
2024-04-07 18:06:39 +0900

    Feat: made FindPw comp UI

--- app/findpw/page.tsx
+++ app/findpw/page.tsx
@@ -1,5 +1,36 @@
+'use client';
+
+import classNames from 'classnames/bind';
+
+import Button from '/components/atoms/Button';
+import SignInput from '/components/atoms/SignInput';
+
+import style from './findpw.module.scss';
+
+const cx = classNames.bind(style);
+
 const FindPw = () => {
-  return null;
+  return (
+    <main className={cx('main')}>
+      <div>
+        <div>비밀번호 찾기</div>
+        <div>
+          비밀번호 재설정을 위해
+          <br />
+          이름과 이메일 입력이 필요해요.
+        </div>
+      </div>
+      <form className={cx('auth-get-form')}>
+        <SignInput label="이름" required />
+        <SignInput label="이메일" required />
+        <Button content="인증번호 받기" />
+      </form>
+      <form className={cx('auth-send-form')}>
+        <SignInput label="인증번호" required />
+        <Button content="확인" disabled />
+      </form>
+    </main>
+  );
 };
 
 export default FindPw;
