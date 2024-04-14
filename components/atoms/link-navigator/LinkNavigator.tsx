d568e90192d307e54946f6a87243db84ac7bac33
jdy8739 <jdy8739@naver.com>
2024-04-15 00:18:49 +0900

    Feat: made a basic UI of LinkNavigator comp

---
+++ components/atoms/link-navigator/LinkNavigator.tsx
@@ -0,0 +1,33 @@
+import classNames from 'classnames/bind';
+
+import style from './LinkNavigator.module.scss';
+import { NavLeftSmall, NavRightSmall } from '/assets/svg';
+
+const cx = classNames.bind(style);
+
+interface Props {
+  className?: string;
+  title: string;
+  subTitle: string;
+}
+
+const LinkNavigator = ({ className, title, subTitle }: Props) => {
+  return (
+    <div className={cx('wrapper', className)}>
+      <span className={cx('nav-text')}>
+        <span>{title}</span>
+        <span>{subTitle}</span>
+      </span>
+      <span className={cx('nav-button')}>
+        <button type="button">
+          <NavLeftSmall />
+        </button>
+        <button type="button">
+          <NavRightSmall />
+        </button>
+      </span>
+    </div>
+  );
+};
+
+export default LinkNavigator;
