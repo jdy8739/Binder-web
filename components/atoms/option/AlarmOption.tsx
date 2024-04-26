c6cab88ee6be33ac6bf356dc2fcc58d454e34244
jdy8739 <jdy8739@naver.com>
2024-04-26 13:40:41 +0900

    Feat: made AlarmOption comp for noti icon

---
+++ components/atoms/option/AlarmOption.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import classNames from 'classnames/bind';
+
+import Option, { BasicOption } from './Option';
+
+import style from './AlarmOption.module.scss';
+
+const cx = classNames.bind(style);
+
+interface IAlarmOption extends BasicOption {
+  time: number;
+  clicked?: boolean;
+}
+
+interface AlarmOptionProps {
+  className?: string;
+  option: IAlarmOption;
+}
+
+const AlarmOption = ({ className, option }: AlarmOptionProps) => {
+  return (
+    <div className={cx('wrapper', { clicked: option.clicked }, className)}>
+      <Link className={cx('label')} href="/#">
+        <Option option={option} />
+      </Link>
+      <span className={cx('time')}>{option.time}</span>
+    </div>
+  );
+};
+
+AlarmOption.displayName = 'Option';
+
+export default AlarmOption;
