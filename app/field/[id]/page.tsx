875338930426ae7f8cb338a3e38b4ccf0889e506
jdy8739 <jdy8739@naver.com>
2024-05-15 11:28:30 +0900

    Style: made FiledDetail upper UI

--- app/field/[id]/page.tsx
+++ app/field/[id]/page.tsx
@@ -1,23 +1,56 @@
 import classNames from 'classnames/bind';
 
+import BorderedButton from '/components/atoms/button/BorderedButton';
+import Button from '/components/atoms/button/Button';
+import Radio from '/components/atoms/radio/Radio';
+
 import style from './fieldDetail.module.scss';
 
 const cx = classNames.bind(style);
 
 const FiledDetailTop = () => {
   return (
-    <article className={cx('filed-detail-top')}>
+    <article className={cx('field-detail-top')}>
       <figure>
-        <div>암생물학 필드 태그 검색결과</div>
+        <div className={cx('field-detail-top-title')}>
+          <span>&apos;암생물학&apos;</span>
+          <span>필드 태그 검색결과</span>
+        </div>
       </figure>
       <figure>
-        <div>buttons</div>
+        <div className={cx('field-detail-top-categories')}>
+          <BorderedButton content="직무 게시판" chosen />
+          <BorderedButton content="학술 게시판" />
+          <BorderedButton content="취직이직 게시판" />
+        </div>
       </figure>
       <figure>
-        <div>직무게시판 1,999건</div>
+        <div className={cx('field-detail-top-total-count')}>
+          <span>직무게시판</span>
+          <span>1,999</span>
+          <span>건</span>
+        </div>
       </figure>
       <figure>
-        <div>게시글 작성</div>
+        <div className={cx('field-detail-top-lower')}>
+          <div>
+            <span className={cx('radio')}>
+              <Radio name="filter" defaultChecked />
+              <span>최신순</span>
+            </span>
+            <span className={cx('radio')}>
+              <Radio name="filter" />
+              <span>추천순</span>
+            </span>
+            <span className={cx('radio')}>
+              <Radio name="filter" />
+              <span>조회수</span>
+            </span>
+          </div>
+          <div>
+            <Button content="게시글 작성" size="sm" />
+          </div>
+        </div>
       </figure>
     </article>
   );
