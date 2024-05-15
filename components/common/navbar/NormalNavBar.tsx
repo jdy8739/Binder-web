1bad27f22cfc3658ddc4cdb356d392306759136a
jdy8739 <jdy8739@naver.com>
2024-05-15 13:46:55 +0900

    Refac: call NormalNavBar from field-const in NormalNavBar const

--- components/common/navbar/NormalNavBar.tsx
+++ components/common/navbar/NormalNavBar.tsx
@@ -9,6 +9,8 @@ import AlarmOption from '/components/atoms/option/AlarmOption';
 import LinkOption from '../../atoms/option/LinkOption';
 import { Bell, Pen, Profile } from '/assets/svg';
 
+import { FILED_CONST } from '/business/const/field-const';
+
 import style from './NormalNavBar.module.scss';
 
 const cx = classNames.bind(style);
@@ -153,21 +155,7 @@ const NormalNavBar = () => {
                 </span>
               }
               optionComponent={LinkOption}
-              optionList={[
-                { value: 'life-science', label: '암생물학' },
-                { value: 'virus', label: '바이러스' },
-                { value: 'sales', label: '영업' },
-                { value: 'bacteria', label: '박테리아' },
-                { value: 'genetics', label: '유전학' },
-                { value: 'bioinformatics', label: '생명정보' },
-                { value: 'medical-equipment', label: '의료기기' },
-                { value: 'smart-farm', label: '스마트팜' },
-                { value: 'pharmaceutical-engineering', label: '제약공학' },
-                { value: 'gardening', label: '식물농업' },
-                { value: 'stem-cells', label: '줄기세포' },
-                { value: 'food-engineering', label: '식품공학' },
-                { value: 'livestock-industry', label: '축산*가축' },
-              ]}
+              optionList={FILED_CONST}
               effect="fade"
             />
           </li>
