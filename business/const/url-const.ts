b7e352f605f5c40e30740e92f2012f224ec35eb2
jdy8739 <jdy8739@naver.com>
2024-06-24 00:04:32 +0900

    Feat: add url link in NavBar board options

--- business/const/url-const.ts
+++ business/const/url-const.ts
@@ -3,6 +3,12 @@ const URL_CONST = {
   SIGN_IN: '/signin',
   FIND_PW: '/findpw',
   RESET_PW: '/resetpw',
+
+  BOARD: {
+    DEPT: '/board/dept',
+    ARTS: '/board/arts',
+    JOBS: '/board/jobs',
+  },
 } as const;
 
 // eslint-disable-next-line import/prefer-default-export
