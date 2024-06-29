290632432a046c5c9ef7cfc82d8a5525fe46d96b
jdy8739 <jdy8739@naver.com>
2024-06-29 22:31:56 +0900

    Feat: add function getHtmlStyle in domUtil

--- business/helper/domUtils.ts
+++ business/helper/domUtils.ts
@@ -1,10 +1,26 @@
+import { setInSesstionStorage } from './storageUtils';
+
 const htmlElement =
   typeof window === 'undefined'
     ? null
     : document.getElementsByTagName('html')[0];
 
+const getHtmlStyle = (key: string) => {
+  if (htmlElement) {
+    const htmlStyle = getComputedStyle(htmlElement);
+
+    return htmlStyle[key];
+  }
+
+  return '';
+};
+
 const freezeWindowScroll = () => {
   if (htmlElement) {
+    const htmlHeight = getHtmlStyle('height');
+
+    setInSesstionStorage('htmlHeight', htmlHeight);
+
     htmlElement.style.height = '100vh';
     htmlElement.style.overflow = 'hidden';
   }
