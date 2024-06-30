8614959dc7986b0c1bd87a71f71e361f6a8afdcf
jdy8739 <jdy8739@naver.com>
2024-06-30 11:14:22 +0900

    Feat: scroll release when popModal called and modalLength shorter or equal than 1

--- business/helper/modalUtils.ts
+++ business/helper/modalUtils.ts
@@ -2,19 +2,28 @@ import { ModalType } from '/components/common/modal/Modal';
 
 import modalController from '../class/ModalController';
 
-import { freezeWindowScroll } from './domUtils';
+import { freezeWindowScroll, releaseWindowScroll } from './domUtils';
 
 const addModal = (modal: ModalType) => {
   modalController.addModal(modal);
+
   freezeWindowScroll();
 };
 
 const resetModal = () => {
   modalController.resetModal();
+
+  releaseWindowScroll();
 };
 
 const popModal = () => {
   modalController.popModal();
+
+  const modalLength = modalController.getModalListLength();
+
+  if (modalLength <= 1) {
+    releaseWindowScroll();
+  }
 };
 
 export { addModal, resetModal, popModal };
