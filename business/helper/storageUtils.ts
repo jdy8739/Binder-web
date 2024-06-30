7dc4b755e2650c1216402f7005c89c5a2fb01ba0
jdy8739 <jdy8739@naver.com>
2024-06-30 11:14:56 +0900

    Refac: change params type to string rest array of all functions in storageUtils

--- business/helper/storageUtils.ts
+++ business/helper/storageUtils.ts
@@ -1,13 +1,28 @@
-const setInLocalStorage = (key: string, value: string) => {
-  window.localStorage.setItem(key, value);
+const setInLocalStorage = (obejct: Record<string, string>) => {
+  Object.keys(obejct).forEach((key) => {
+    window.localStorage.setItem(key, obejct[key]);
+  });
 };
 
-const setInSesstionStorage = (key: string, value: string) => {
-  window.sessionStorage.setItem(key, value);
+const setInSesstionStorage = (obejct: Record<string, string>) => {
+  Object.keys(obejct).forEach((key) => {
+    window.sessionStorage.setItem(key, obejct[key]);
+  });
 };
 
-const getInSesstionStorage = (key: string) => {
-  return window.sessionStorage.getItem(key);
+const getInSesstionStorage = (...key: string[]) => {
+  return Object.fromEntries(
+    key.map((el) => [el, window.sessionStorage.getItem(el)]),
+  );
 };
 
-export { setInLocalStorage, setInSesstionStorage, getInSesstionStorage };
+const deleteInSessionStorage = (...key: string[]) => {
+  key.forEach((el) => window.sessionStorage.removeItem(el));
+};
+
+export {
+  setInLocalStorage,
+  setInSesstionStorage,
+  getInSesstionStorage,
+  deleteInSessionStorage,
+};
