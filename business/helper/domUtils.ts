a62a216b10e57fc97b5b906fb59a9b32ea5fccc3
jdy8739 <jdy8739@naver.com>
2024-06-30 11:13:27 +0900

    Feat: scroll, htmlStyle functions in domUtils

--- business/helper/domUtils.ts
+++ business/helper/domUtils.ts
@@ -1,33 +1,69 @@
-import { setInSesstionStorage } from './storageUtils';
+import {
+  deleteInSessionStorage,
+  getInSesstionStorage,
+  setInSesstionStorage,
+} from './storageUtils';
 
 const htmlElement =
   typeof window === 'undefined'
     ? null
     : document.getElementsByTagName('html')[0];
 
-const getHtmlStyle = (key: string) => {
+const getHtmlStyle = (...key: string[]) => {
   if (htmlElement) {
     const htmlStyle = getComputedStyle(htmlElement);
 
-    return htmlStyle[key];
+    return Object.fromEntries(key.map((el) => [el, htmlStyle[el]]));
   }
 
-  return '';
+  return {};
+};
+
+const setHtmlStyle = (styles: Record<string, string>) => {
+  if (htmlElement) {
+    const htmlStyle = htmlElement.style;
+
+    Object.keys(styles).forEach((key) => {
+      htmlStyle[key] = styles[key];
+    });
+  }
 };
 
 const freezeWindowScroll = () => {
   if (htmlElement) {
-    const htmlHeight = getHtmlStyle('height');
+    const { htmlWidth: storagedWidth, htmlHeight: storagedHeight } =
+      getInSesstionStorage('htmlWidth', 'htmlHeight');
 
-    setInSesstionStorage('htmlHeight', htmlHeight);
+    const { width: htmlWidth, height: htmlHeight } = getHtmlStyle(
+      'width',
+      'height',
+    );
 
-    htmlElement.style.height = '100vh';
-    htmlElement.style.overflow = 'hidden';
+    if (!storagedWidth && !storagedHeight) {
+      setInSesstionStorage({ htmlWidth, htmlHeight });
+    }
+
+    setHtmlStyle({ width: '100vw', height: '100vh', overflow: 'hidden' });
   }
 };
 
 const releaseWindowScroll = () => {
-  //
+  if (htmlElement) {
+    const { htmlWidth, htmlHeight } = getInSesstionStorage(
+      'htmlWidth',
+      'htmlHeight',
+    );
+
+    if (htmlWidth && htmlHeight) {
+      setHtmlStyle({
+        width: htmlWidth,
+        height: htmlHeight,
+        overflow: 'scroll',
+      });
+
+      deleteInSessionStorage('htmlWidth', 'htmlHeight');
+    }
+  }
 };
 
 export { freezeWindowScroll, releaseWindowScroll };
