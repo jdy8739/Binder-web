5b64861337c78456c6f6f489ed7743a05fc10a42
jdy8739 <jdy8739@naver.com>
2024-07-01 00:18:58 +0900

    Refac: remove useless dom controll

--- business/helper/domUtils.ts
+++ business/helper/domUtils.ts
@@ -1,69 +1,14 @@
-import {
-  deleteInSessionStorage,
-  getInSesstionStorage,
-  setInSesstionStorage,
-} from './storageUtils';
-
 const htmlElement =
   typeof window === 'undefined'
     ? null
     : document.getElementsByTagName('html')[0];
 
-const getHtmlStyle = (...key: string[]) => {
-  if (htmlElement) {
-    const htmlStyle = getComputedStyle(htmlElement);
-
-    return Object.fromEntries(key.map((el) => [el, htmlStyle[el]]));
-  }
-
-  return {};
-};
-
-const setHtmlStyle = (styles: Record<string, string>) => {
-  if (htmlElement) {
-    const htmlStyle = htmlElement.style;
-
-    Object.keys(styles).forEach((key) => {
-      htmlStyle[key] = styles[key];
-    });
-  }
-};
-
 const freezeWindowScroll = () => {
-  if (htmlElement) {
-    const { htmlWidth: storagedWidth, htmlHeight: storagedHeight } =
-      getInSesstionStorage('htmlWidth', 'htmlHeight');
-
-    if (!storagedWidth && !storagedHeight) {
-      const { width: htmlWidth, height: htmlHeight } = getHtmlStyle(
-        'width',
-        'height',
-      );
-
-      setInSesstionStorage({ htmlWidth, htmlHeight });
-    }
-
-    setHtmlStyle({ width: '100vw', height: '100vh', overflow: 'hidden' });
-  }
+  htmlElement?.classList.add('freezed');
 };
 
 const releaseWindowScroll = () => {
-  if (htmlElement) {
-    const { htmlWidth, htmlHeight } = getInSesstionStorage(
-      'htmlWidth',
-      'htmlHeight',
-    );
-
-    if (htmlWidth && htmlHeight) {
-      setHtmlStyle({
-        width: htmlWidth,
-        height: htmlHeight,
-        overflow: 'scroll',
-      });
-
-      deleteInSessionStorage('htmlWidth', 'htmlHeight');
-    }
-  }
+  htmlElement?.classList.remove('freezed');
 };
 
 export { freezeWindowScroll, releaseWindowScroll };
