a95dc91117598b13acde2ac6705ffed736f2a415
jdy8739 <jdy8739@naver.com>
2024-07-01 00:20:05 +0900

    Feat: calculate margin top of modal wrapper

--- components/common/modal/Modal.tsx
+++ components/common/modal/Modal.tsx
@@ -1,14 +1,23 @@
+/* eslint-disable react/jsx-no-useless-fragment */
+
 'use client';
 
 import React, {
   FunctionComponent,
   useCallback,
   useEffect,
+  useLayoutEffect,
+  useRef,
   useState,
 } from 'react';
 import { usePathname } from 'next/navigation';
+import classNames from 'classnames/bind';
 
 import modalController from '/business/class/ModalController';
+
+import style from './Modal.module.scss';
+
+const cx = classNames.bind(style);
 
 type PropsOf<T> = T extends FunctionComponent<infer P> ? P : never;
 
@@ -26,6 +35,8 @@ type AddModalType = <T extends FunctionComponent>(modal: {
 
 const Modal = () => {
   const pathname = usePathname();
+
+  const modalWrapperRef = useRef<HTMLDivElement>(null);
 
   const [modalList, setModalList] = useState<ModalType[]>([]);
 
@@ -70,14 +81,24 @@ const Modal = () => {
     resetModal();
   }, [pathname, resetModal]);
 
+  useLayoutEffect(() => {
+    if (modalList.length === 1 && modalWrapperRef.current) {
+      modalWrapperRef.current.style.marginTop = `${document.documentElement.scrollTop}px`;
+    }
+  }, [modalList]);
+
   return (
     <>
-      {modalList.map((modal) => {
-        return React.createElement(modal.component, {
-          key: modal.id,
-          ...(modal.props || {}),
-        });
-      })}
+      {modalList.length ? (
+        <aside ref={modalWrapperRef} className={cx('modal-wrapper')}>
+          {modalList.map((modal) => {
+            return React.createElement(modal.component, {
+              key: modal.id,
+              ...(modal.props || {}),
+            });
+          })}
+        </aside>
+      ) : null}
     </>
   );
 };
