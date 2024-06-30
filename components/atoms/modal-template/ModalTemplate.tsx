69b3d00b1113a16d1440f30e2e141cdd58cceeed
jdy8739 <jdy8739@naver.com>
2024-06-30 11:15:51 +0900

    Feat: popModal called when users click dim of Modal

--- components/atoms/modal-template/ModalTemplate.tsx
+++ components/atoms/modal-template/ModalTemplate.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 import classNames from 'classnames/bind';
 
 import style from './ModalTemplate.module.scss';
+import { popModal } from '/business/helper/modalUtils';
 
 const cx = classNames.bind(style);
 
@@ -14,10 +15,15 @@ type Props = {
 
 const ModalTemplate = ({ className, children, header, footer }: Props) => {
   return (
-    <div className={cx('modal-template-wrapper', className)}>
-      {header && <div className={cx('modal-template-header')}>{header}</div>}
-      {children}
-      {footer && <div className={cx('modal-template-footer')}>{footer}</div>}
+    <div
+      className={cx('modal-template-wrapper', 'dim', className)}
+      onClick={popModal}
+    >
+      <article className={cx('modal-template-inner')}>
+        {header && <div className={cx('modal-template-header')}>{header}</div>}
+        {children}
+        {footer && <div className={cx('modal-template-footer')}>{footer}</div>}
+      </article>
     </div>
   );
 };
