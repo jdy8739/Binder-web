e4184bc82303de86d671bbc0d14fc28a0db55e7a
jdy8739 <jdy8739@naver.com>
2024-07-07 09:36:24 +0900

    Style: style of answer card comps

--- components/pages/board/post/[id]/post-detail/post-answer/PostAnswer.tsx
+++ components/pages/board/post/[id]/post-detail/post-answer/PostAnswer.tsx
@@ -7,7 +7,7 @@ import style from './PostAnswer.module.scss';
 const cx = classNames.bind(style);
 
 const PostAnswer = () => {
-  return 'post answer';
+  return <div className={cx('card', 'answer')}>1</div>;
 };
 
 export default PostAnswer;
--- components/pages/board/post/[id]/post-detail/post-answering/PostAnswering.tsx
+++ components/pages/board/post/[id]/post-detail/post-answering/PostAnswering.tsx
@@ -2,12 +2,36 @@
 
 import classNames from 'classnames/bind';
 
+import ProfileAvatar from '/components/atoms/profile/ProfileAvatar';
+import TextEditor from '/components/atoms/text-editor/TextEditor';
+import Button from '/components/atoms/button/Button';
+
 import style from './PostAnswering.module.scss';
 
 const cx = classNames.bind(style);
 
 const PostAnswering = () => {
-  return 'post answering';
+  return (
+    <div>
+      <div className={cx('card', 'answer')}>
+        <div>
+          <ProfileAvatar size="md" afterNickname="님, 답변해주세요." />
+        </div>
+        <div>
+          <Button width={160} height={50}>
+            답변 등록
+          </Button>
+        </div>
+      </div>
+      <div>
+        <TextEditor
+          className={cx('answer-editor')}
+          lengthLimitation={false}
+          placeholder={`답변을 작성해주세요. \n답변 작성 시 서비스 운영정책을 지켜주세요.`}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default PostAnswering;
