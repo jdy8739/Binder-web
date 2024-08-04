3ebdc5b1b158512027d6612e25a3a965a1492ecf
minu <wlwhsxz@gmail.com>
2024-08-04 23:00:09 +0900

    feat: add mypage initial draft

--- app/(auth)/auth/[username]/page.tsx
+++ app/(auth)/auth/[username]/page.tsx
@@ -1,33 +1,57 @@
-import React from 'react';
+'use client'
+
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
-import styles from './username.module.scss'
+import styles from './username.module.scss';
 import MySidebar from '../../../../components/pages/my-page/sidebar/MySidebar';
 import MyProfile from '/components/pages/my-page/profile/MyProfile';
+import Badge from '/components/pages/my-page/badge/Badge';
+import Contents from '/components/pages/my-page/contents/Contents';
+import ScrapLikes from '/components/pages/my-page/scrap-likes/ScrapLikes';
+import Notifications from '/components/pages/my-page/notifications/Notifications';
+import CheckBox from '/components/atoms/check-box/CheckBox';
+
 const cx = classNames.bind(styles);
 
+const MyPage = () => {
+  const [selectedMenu, setSelectedMenu] = useState('home');
 
-const page = () => {
+  const renderContent = () => {
+    switch (selectedMenu) {
+      case 'home':
+        return (
+          <>
+            <div className={cx('home-boxes')}>
+              <MyProfile />
+              <Badge />
+            </div>
+            <div className={cx('rest-boxes')}>
+              <Contents />
+              <ScrapLikes />
+            </div>
+          </>
+        );
+      case 'contents':
+        return <Contents />;
+      case 'scrapLikes':
+        return <ScrapLikes />;
+      case 'profileEdit':
+        return <div>개인정보 수정 내용</div>;
+      case 'notifications':
+        return <Notifications />;
+      default:
+        return <div>잘못된 접근입니다.</div>;
+    }
+  };
+
   return (
     <div className={cx('container')}>
-      <MySidebar />
+      <MySidebar selectedMenu={selectedMenu} setSelectedMenu={setSelectedMenu} />
       <div className={cx('main-wrapper')}>
-        <div>
-          <MyProfile />
-        </div>
-        <div>
-          <h3>나의 콘텐츠</h3>
-          <div>나의 글</div>
-          <div>나의 답변</div>
-          <div>나의 댓글</div>
-        </div>
-        <div>
-          <h3>스크랩 좋아요</h3>
-          <div>스크랩</div>
-          <div>좋아요</div>
-          </div>
+        {renderContent()}
       </div>
     </div>
   );
 };
 
-export default page;
+export default MyPage;
