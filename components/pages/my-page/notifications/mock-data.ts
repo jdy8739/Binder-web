c85fe3d55c02b7d986e9125f37e6191b1f64027d
minu <wlwhsxz@gmail.com>
2024-08-04 22:38:54 +0900

    chore: add temporary mock data for notifications

---
+++ components/pages/my-page/notifications/mock-data.ts
@@ -0,0 +1,72 @@
+export const notifications = [
+  {
+    id: "1",
+    title: "New Notification",
+    message: "내가 작성한 게시글에 Aaaasia님이 답글을 작성했습니다.",
+    status: "unread",
+    timestamp: "5분전"
+  },
+  {
+    id: "2",
+    title: "Another Notification",
+    message: "내가 작성한 게시글에 Aaaasia님이 답글을 작성했습니다.",
+    status: "unread",
+    timestamp: "5분전"
+  },
+  {
+    id: "3",
+    title: "Another Notification",
+    message: "내가 작성한 게시글에 Aaaasia님이 답글을 작성했습니다.",
+    status: "unread",
+    timestamp: "5분전"
+  },
+  {
+    id: "4",
+    title: "Another Notification",
+    message: "내가 작성한 게시글에 Aaaasia님이 답글을 작성했습니다.",
+    status: "unread",
+    timestamp: "5분전"
+  },
+  {
+    id: "5",
+    title: "Another Notification",
+    message: "내가 작성한 게시글에 Aaaasia님이 답글을 작성했습니다.",
+    status: "read",
+    timestamp: "2일전"
+  },
+  {
+    id: "6",
+    title: "Another Notification",
+    message: "내가 작성한 게시글에 Aaaasia님이 답글을 작성했습니다.",
+    status: "read",
+    timestamp: "2일전"
+  },
+  {
+    id: "7",
+    title: "Another Notification",
+    message: "내가 작성한 게시글에 Aaaasia님이 답글을 작성했습니다.",
+    status: "read",
+    timestamp: "2일전"
+  },
+  {
+    id: "8",
+    title: "Another Notification",
+    message: "내가 작성한 게시글에 Aaaasia님이 답글을 작성했습니다.",
+    status: "read",
+    timestamp: "2일전"
+  },
+  {
+    id: "9",
+    title: "Another Notification",
+    message: "내가 작성한 게시글에 Aaaasia님이 답글을 작성했습니다.",
+    status: "read",
+    timestamp: "2일전"
+  },
+  {
+    id: "10",
+    title: "Another Notification",
+    message: "내가 작성한 게시글에 Aaaasia님이 답글을 작성했습니다.",
+    status: "read",
+    timestamp: "2일전"
+  }
+];
