692fdf8c7b382a50111bcd8ab46d6a159acabe19
minu <wlwhsxz@gmail.com>
2024-08-04 23:03:45 +0900

    feat: add sidebar for my page

--- components/pages/my-page/sidebar/MySidebar.tsx
+++ components/pages/my-page/sidebar/MySidebar.tsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import classNames from 'classnames/bind'
-import styles from './MySidebar.module.scss'
-const cx = classNames.bind(styles)
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './MySidebar.module.scss';
+const cx = classNames.bind(styles);
 
-const MySidebar = () => {
+const MySidebar = ({ selectedMenu, setSelectedMenu }) => {
   return (
     <nav className={cx('sidebar')}>
       <ul>
-        <li><a href="#">MY 홈</a></li>
-        <li><a href="#">나의 콘텐츠</a></li>
-        <li><a href="#">스크랩/좋아요</a></li>
-        <li><a href="#">개인정보 수정</a></li>
-        <li><a href="#">나의 알림</a></li>
-        <li><a href="#">로그아웃</a></li>
+        <li className={cx({ selected: selectedMenu === 'home' })}>
+          <a href="#" onClick={() => setSelectedMenu('home')}>MY 홈</a>
+        </li>
+        <li className={cx({ selected: selectedMenu === 'contents' })}>
+          <a href="#" onClick={() => setSelectedMenu('contents')}>나의 콘텐츠</a>
+        </li>
+        <li className={cx({ selected: selectedMenu === 'scrapLikes' })}>
+          <a href="#" onClick={() => setSelectedMenu('scrapLikes')}>스크랩/좋아요</a>
+        </li>
+        <li className={cx({ selected: selectedMenu === 'profileEdit' })}>
+          <a href="#" onClick={() => setSelectedMenu('profileEdit')}>개인정보 수정</a>
+        </li>
+        <li className={cx({ selected: selectedMenu === 'notifications' })}>
+          <a href="#" onClick={() => setSelectedMenu('notifications')}>나의 알림</a>
+        </li>
+        <li>
+          <a href="#">로그아웃</a>
+        </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default MySidebar
+export default MySidebar;
