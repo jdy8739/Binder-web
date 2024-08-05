6406041bf6ef4fa8dc3edd7f34427c634fae39c7
minu <wlwhsxz@gmail.com>
2024-08-05 22:43:26 +0900

    chore: modify mock-data

--- components/pages/my-page/contents/mock-data.ts
+++ components/pages/my-page/contents/mock-data.ts
@@ -3,156 +3,468 @@ export const boards = [
         boardType: '직무게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
     {
         boardType: '직무게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
     {
         boardType: '직무게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
     {
         boardType: '학술게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
     {
         boardType: '취직-이직게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
     {
         boardType: '취직-이직게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
     {
         boardType: '취직-이직게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
     {
         boardType: '취직-이직게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
     {
         boardType: '취직-이직게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
     {
         boardType: '취직-이직게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
     {
         boardType: '취직-이직게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
     {
         boardType: '취직-이직게시판',
         title: '게시글 제목',
         content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
-        likes: 4,
+        likes: [
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            },
+            {
+                id: 'hello',
+            }
+        ],
         commnets: [
             {
                 id: 'hello',
                 content: 'blah blah...',
-            }
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            },
         ],
         views: 989,
+        createdAt: '2024-08-05'
     },
 ]
