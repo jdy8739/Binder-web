808104476604cadd4142e0df50a1cce0fbb6d37b
Minu <wlwhsxz@gmail.com>
2024-08-05 18:57:00 +0900

    chore: modify extension css to scss

--- components/atoms/hashtag/Hashtag.tsx
+++ components/atoms/hashtag/Hashtag.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import classNames from 'classnames/bind';
-import styles from './Hashtag.module.css';
+import styles from './Hashtag.module.scss';
 const cx = classNames.bind(styles);
 
 
