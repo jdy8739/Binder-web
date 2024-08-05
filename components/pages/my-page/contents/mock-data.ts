d20801a81e5e47bf8c8c2038377923df9e9c5053
Minu <wlwhsxz@gmail.com>
2024-08-05 19:02:31 +0900

    chore: mock data form contents component

---
+++ components/pages/my-page/contents/mock-data.ts
@@ -0,0 +1,158 @@
+export const boards = [
+    {
+        boardType: '직무게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+    {
+        boardType: '직무게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+    {
+        boardType: '직무게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+    {
+        boardType: '학술게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+    {
+        boardType: '취직-이직게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+    {
+        boardType: '취직-이직게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+    {
+        boardType: '취직-이직게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+    {
+        boardType: '취직-이직게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+    {
+        boardType: '취직-이직게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+    {
+        boardType: '취직-이직게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+    {
+        boardType: '취직-이직게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+    {
+        boardType: '취직-이직게시판',
+        title: '게시글 제목',
+        content: '게시글 작성곤간입니다. 게시글 작성공간입니다. 게시글 작성공간입니다.',
+        likes: 4,
+        commnets: [
+            {
+                id: 'hello',
+                content: 'blah blah...',
+            }
+        ],
+        views: 989,
+    },
+]
