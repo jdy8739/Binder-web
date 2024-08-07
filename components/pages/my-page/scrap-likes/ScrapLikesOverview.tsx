a14c3c7b7b0afd70655c517ba2158044fe5c47e9
jdy8739 <jdy8739@naver.com>
2024-08-07 23:55:23 +0900

    Fix: uninstall eslint

--- components/pages/my-page/scrap-likes/ScrapLikesOverview.tsx
+++ components/pages/my-page/scrap-likes/ScrapLikesOverview.tsx
@@ -1,36 +1,37 @@
-import React from 'react'
+import React from 'react';
 import classNames from 'classnames/bind';
-import styles from './ScrapLikesOverview.module.scss'
+import styles from './ScrapLikesOverview.module.scss';
+import ContentBox from '/components/atoms/content-box/ContentBox';
+
 const cx = classNames.bind(styles);
-import ContentBox from '/components/atoms/content-box/ContentBox'
 
 const ScrapLikesOverview = () => {
   return (
     <div className={cx('scrap-likes')}>
       <p>스크랩/좋아요</p>
       <div className={cx('scrap-like-boxes')}>
-          <ContentBox
-                  title="스크랩"
-                  subtitle1="스크랩한 글이 없어요"
-                  subtitle2="원하는 콘텐츠를 저장해보세요"
-                  buttonText="스크랩 전체보기"
-                  buttonLink="/"
-                  buttonWidth={305}
-                  buttonSize="lg"
-          />
-          <ContentBox
-                  title="좋아요"
-                  subtitle1="좋아요한 없어요"
-                  subtitle2="원하는 콘텐츠를 저장해보세요"
-                  buttonText="좋아요 전체보기"
-                  buttonLink="/"
-                  buttonWidth={305}
-                  buttonSize="lg"
-          />
-          <div style={{ display: 'none' }}></div>
+        <ContentBox
+          title="스크랩"
+          subtitle1="스크랩한 글이 없어요"
+          subtitle2="원하는 콘텐츠를 저장해보세요"
+          buttonText="스크랩 전체보기"
+          buttonLink="/"
+          buttonWidth={305}
+          buttonSize="lg"
+        />
+        <ContentBox
+          title="좋아요"
+          subtitle1="좋아요한 없어요"
+          subtitle2="원하는 콘텐츠를 저장해보세요"
+          buttonText="좋아요 전체보기"
+          buttonLink="/"
+          buttonWidth={305}
+          buttonSize="lg"
+        />
+        <div style={{ display: 'none' }} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ScrapLikesOverview
+export default ScrapLikesOverview;
